8390fec1052c98aa5f515d0ebc585d80088c2fd3
Tomasz Kwolek <pastezzz@gmail.com>
2013-08-12 19:48:19 +0200

    comments added

--- PSCroopToolComponent/PSCroopToolView.h
+++ PSCroopToolComponent/PSCroopToolView.h
@@ -10,16 +10,16 @@
 
 @interface PSCroopToolView : UIView
 
-@property (readwrite,nonatomic) CGFloat croopAreaFillFactor;
-@property (readwrite,nonatomic) CGFloat requiredFillFactor;
-@property (readwrite,nonatomic) CGSize outputSize;
-@property (readwrite,nonatomic) BOOL showQueueActivitiIndicator;
+@property (readwrite,nonatomic) CGFloat croopAreaFillFactor;            // scale of crop area from 0 to 1. 1 is full component frame
+@property (readwrite,nonatomic) CGFloat requiredFillFactor;             // minimal area of image in croop area
+@property (readwrite,nonatomic) CGSize outputSize;                      // designated output image size
+@property (readwrite,nonatomic) BOOL showQueueActivitiIndicator;        // show UIActivitiIndicatorView when doing getOutputImageAsync
 
-- (void)imageToCroop:(UIImage*)image;
-- (UIImage*)getOutputImage;
-- (void)getOutputImageAsync:(void (^) (UIImage *outputImage))handler;
+- (void)imageToCroop:(UIImage*)image;                                   // sets source image
+- (UIImage*)getOutputImage;                                             // returns output croped image
+- (void)getOutputImageAsync:(void (^) (UIImage *outputImage))handler;   // returns output croped image, croping is made in bacground queue
 
-- (void)setCroopAreaBorderWidth:(CGFloat)width;
-- (void)setCroopAreaBorderColor:(UIColor*)color;
+- (void)setCroopAreaBorderWidth:(CGFloat)width;                         // sets croop area border size
+- (void)setCroopAreaBorderColor:(UIColor*)color;                        // sets croop area border color
 
 @end
